3a0cbb1f7d7614a979d542f7c72be36729f4118c
MiracleKAI <1531996166@qq.com>
2020-06-08 09:50:50 +0800

    Initial commit

---
+++ src/main/java/com/miracle/studentscoremanage/StudentScoreManageApplication.java
@@ -0,0 +1,16 @@
+package com.miracle.studentscoremanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author miracle
+ */
+@SpringBootApplication
+public class StudentScoreManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentScoreManageApplication.class, args);
+    }
+
+}
